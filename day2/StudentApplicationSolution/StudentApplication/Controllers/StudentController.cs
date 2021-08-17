21d0ea649fc02f72ead84e91ad2bb45ee8af0f49
Martina Čilag <mcilag@mathos.hr>
2021-08-17 17:11:49 +0200

    day2

---
+++ day2/StudentApplicationSolution/StudentApplication/Controllers/StudentController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StudentApplication.Controllers
+{
+
+    public class DictionaryofStudents
+    {
+        public static Dictionary<int, string> students = new Dictionary<int, string>();
+
+    }
+
+    public class StudentController : ApiController
+    {
+ 
+        // GET 
+        [HttpGet]
+        [Route("api/Student")]
+        public HttpResponseMessage Get()
+        {
+            
+            if(DictionaryofStudents.students.Count == 0)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "There are no students");
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, DictionaryofStudents.students);
+            }
+        }
+
+        // GET with id
+        [HttpGet]
+        [Route("api/Student/{id}")]
+        public HttpResponseMessage Get(int id)
+        {
+            if(DictionaryofStudents.students.Count() < id)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "There is no student with id: " + Convert.ToString(id));
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, DictionaryofStudents.students[id]);
+            }
+        }
+
+        // POST 
+        [HttpPost]
+        [Route("api/Student/{id}")]
+        public HttpResponseMessage Post(int id, [FromBody] string value)
+        {
+            if (DictionaryofStudents.students.Count() < id)
+            {
+                DictionaryofStudents.students.Add(id, value);
+                return Request.CreateResponse(HttpStatusCode.OK, "Student with id " + Convert.ToString(id) + " is successfully added");
+               
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Student with id " + Convert.ToString(id) + " already exists");
+            }
+        }
+
+        // PUT 
+        [HttpPut]
+        [Route("api/Student/{id}")]
+        public HttpResponseMessage Put(int id, [FromBody] string value)
+        {
+            if (DictionaryofStudents.students.Count() < id)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "There is no student with id: " + Convert.ToString(id));
+            }
+            else
+            {
+                DictionaryofStudents.students[id] = value;
+                return Request.CreateResponse(HttpStatusCode.OK, "Student with id " + Convert.ToString(id) + " is modified");
+            }
+        }
+
+        // DELETE 
+        [HttpDelete]
+        [Route("api/Student/{id}")]
+        public HttpResponseMessage Delete(int id)
+        {
+            if (DictionaryofStudents.students.Count() < id)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "There is no student with id: " + Convert.ToString(id));
+            }
+            else
+            {
+                DictionaryofStudents.students.Remove(id);
+                return Request.CreateResponse(HttpStatusCode.OK, "Student with id " + Convert.ToString(id) + " is deleted");
+            }
+        }
+    }
+
+   
+
+
+}
+
---
+++ day2/StudentApplicationSolution/StudentApplication/Global.asax.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using StudentApplication.Controllers;
+
+namespace StudentApplication
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
+            DictionaryofStudents.students.Add(1, "James Jackson");
+            DictionaryofStudents.students.Add(2, "Marco Black");
+            DictionaryofStudents.students.Add(3, "Steve James");
+            DictionaryofStudents.students.Add(4, "Jane Doe");
+            DictionaryofStudents.students.Add(5, "Josh Peterson");
+            DictionaryofStudents.students.Add(6, "Mark Mark");
+            DictionaryofStudents.students.Add(7, "Jason Jon");
+            DictionaryofStudents.students.Add(8, "Martha South");
+            DictionaryofStudents.students.Add(9, "Nick North");
+            DictionaryofStudents.students.Add(10, "Amanda Nicholson");
+            
+        }
+    }
+}
