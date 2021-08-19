5cef006a456551ab2fb45ef6a6ea379bd4731225
Martina Čilag <mcilag@mathos.hr>
2021-08-19 13:12:10 +0200

    completed

--- day3/StudentApplicationSolution/StudentApplication/Controllers/AdressController.cs
+++ day3/StudentApplicationSolution/StudentApplication/Controllers/AdressController.cs
@@ -17,48 +17,227 @@ namespace AdressApplication.Controllers
 
     public class AdressController : ApiController
     {
-   
-    SqlConnection connection = new SqlConnection("Data Source=DESKTOP-BB9OT7S\\SQLEXPRESS;Initial Catalog = Martina; Integrated Security = True");
 
-    List<Adress> list_of_adresses = new List<Adress>();
+        SqlConnection connection = new SqlConnection("Data Source=DESKTOP-BB9OT7S\\SQLEXPRESS;Initial Catalog = Martina; Integrated Security = True");
 
-    // GET 
-    [HttpGet]
-    [Route("api/Adress")]
-    public HttpResponseMessage Get()
-    {
-        using (connection)
+        List<Adress> list_of_adresses = new List<Adress>();
+
+        // GET 
+        [HttpGet]
+        [Route("api/Adress")]
+        public HttpResponseMessage Get()
         {
-            SqlCommand command = new SqlCommand
-                ("SELECT * FROM Adress;", connection);
-            connection.Open();
-
-            SqlDataReader reader = command.ExecuteReader();
-
-            if (reader.HasRows)
+            using (connection)
             {
-                while (reader.Read())
+                SqlCommand command = new SqlCommand
+                    ("SELECT * FROM Adress;", connection);
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
                 {
-                    Adress adress = new Adress();
-                    adress.adress_id = reader.GetInt32(0);
-                    adress.street = reader.GetString(1);
-                    adress.number = reader.GetInt32(2);
-                    adress.city = reader.GetString(3);
-                    adress.zipcode = reader.GetString(4);
+                    while (reader.Read())
+                    {
+                        Adress adress = new Adress();
+                        adress.adress_id = reader.GetInt32(0);
+                        adress.street = reader.GetString(1);
+                        adress.number = reader.GetInt32(2);
+                        adress.city = reader.GetString(3);
+                        adress.zipcode = reader.GetString(4);
 
 
-                    list_of_adresses.Add(adress);
+                        list_of_adresses.Add(adress);
+                    }
+                    connection.Close();
+                    return Request.CreateResponse(HttpStatusCode.OK, list_of_adresses);
                 }
-                connection.Close();
-                return Request.CreateResponse(HttpStatusCode.OK, list_of_adresses);
+                else
+                {
+                    connection.Close();
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "There are no adresses");
+                }
+
             }
-            else
+        }
+        // GET by id
+        [HttpGet]
+        [Route("api/Adress/{Adress_id}")]
+        public HttpResponseMessage Get(int Adress_id)
+        {
+            using (connection)
             {
-                connection.Close();
-                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "There are no adresses");
+                connection.Open();
+                SqlCommand command = new SqlCommand
+                    ("SELECT * FROM Adress WHERE Adress_id = @Adress_id;", connection);
+
+                SqlParameter id_parameter = new SqlParameter("@Adress_id", System.Data.SqlDbType.Int);
+
+                command.Parameters.Add(id_parameter).Value = Adress_id;
+
+
+
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Adress adress = new Adress();
+                        adress.adress_id = Adress_id;
+                        adress.street = reader.GetString(1);
+                        adress.number = reader.GetInt32(2);
+                        adress.city = reader.GetString(3);
+                        adress.zipcode = reader.GetString(4);
+
+                        list_of_adresses.Add(adress);
+                    }
+                    connection.Close();
+
+                    return Request.CreateResponse(HttpStatusCode.OK, list_of_adresses);
+                }
+                else
+                {
+                    connection.Close();
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "There is no adress with id " + Convert.ToString(Adress_id));
+                }
+            }
+        }
+
+        // POST 
+        [HttpPost]
+        [Route("api/Adress/{Adress_id}/{street}/{number}/{city}/{zipcode}")]
+        public HttpResponseMessage Post(int adress_id, string street, int number, string city, string zipcode)
+        {
+            using (connection)
+            {
+                connection.Open();
+                string queryString = "INSERT INTO Adress VALUES (@Adress_id, @street, @number, @city, @zipcode);";
+
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                command.Parameters.Add("@Adress_id", System.Data.SqlDbType.Int).Value = adress_id;
+                command.Parameters.Add("@street", System.Data.SqlDbType.VarChar, 50).Value = street;
+                command.Parameters.Add("@number", System.Data.SqlDbType.Int).Value = number;
+                command.Parameters.Add("@city", System.Data.SqlDbType.VarChar, 50).Value = city;
+                command.Parameters.Add("@zipcode", System.Data.SqlDbType.VarChar, 50).Value = zipcode;
+
+                SqlCommand command_check = new SqlCommand
+                   ("SELECT Adress_id FROM Adress WHERE adress_id = @Adress_id;", connection);
+
+                command_check.Parameters.Add("@Adress_id", System.Data.SqlDbType.Int).Value = adress_id;
+
+                SqlDataReader reader = command_check.ExecuteReader();
+
+
+                if (!reader.HasRows)
+                {
+                    connection.Close();
+                    connection.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter();
+                    adapter.InsertCommand = command;
+                    adapter.InsertCommand.ExecuteNonQuery();
+                    connection.Close();
+                    return Request.CreateResponse(HttpStatusCode.OK, "Adress is successfully added");
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "You can not add adress with already existing id.");
+                }
             }
 
         }
+
+        // PUT for given id modifies city
+        [HttpPut]
+        [Route("api/Adress/{adress_id}")]
+        public HttpResponseMessage Put(int adress_id, [FromBody] string city)
+        {
+            using (connection)
+            {
+
+                connection.Open();
+
+                SqlCommand command_check_pk = new SqlCommand
+                        ("SELECT Adress_id FROM Adress WHERE Adress_id = @adress_id;", connection);
+
+                command_check_pk.Parameters.Add("@adress_id", System.Data.SqlDbType.Int).Value = adress_id;
+
+                SqlDataReader reader_pk = command_check_pk.ExecuteReader();
+
+                if (reader_pk.HasRows)
+                {
+                    connection.Close();
+                    connection.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter("SELECT Adress_id, City FROM Adress;", connection);
+                    adapter.UpdateCommand = new SqlCommand("UPDATE Adress SET City = @city;", connection);
+
+                    adapter.UpdateCommand.Parameters.Add("@city", System.Data.SqlDbType.VarChar).Value = city;
+         
+
+                    SqlParameter parameter = adapter.UpdateCommand.Parameters.Add("@adress_id", System.Data.SqlDbType.Int);
+                    parameter.SourceColumn = "Adress_id";
+                    parameter.SourceVersion = System.Data.DataRowVersion.Original;
+
+                    System.Data.DataTable categoryTable = new System.Data.DataTable();
+                    adapter.Fill(categoryTable);
+
+                    System.Data.DataRow categoryRow = categoryTable.Rows[3];
+                    categoryRow["City"] = city;
+
+
+                    adapter.Update(categoryTable);
+
+                    connection.Close();
+                    return Request.CreateResponse(HttpStatusCode.OK, "Adress is successfully modified");
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "There is no adress with id " + Convert.ToString(adress_id));
+                }
+
+            }
+        }
+        // DELETE 
+        [HttpDelete]
+        [Route("api/Adress/{adress_id}")]
+        public HttpResponseMessage Delete(int adress_id)
+        {
+            using (connection)
+            {
+                connection.Open();
+
+                SqlCommand command_check_pk = new SqlCommand
+                        ("SELECT Adress_id FROM Adress WHERE Adress_id = @adress_id;", connection);
+
+                command_check_pk.Parameters.Add("@Adress_id", System.Data.SqlDbType.Int).Value = adress_id;
+
+                SqlDataReader reader_pk = command_check_pk.ExecuteReader();
+
+                if (reader_pk.HasRows)
+                {
+                    connection.Close();
+                    connection.Open();
+
+                    SqlDataAdapter adapter = new SqlDataAdapter();
+
+                    adapter.DeleteCommand = new SqlCommand("DELETE FROM Adress WHERE Adress_id = @adress_id ", connection);
+
+                    adapter.DeleteCommand.Parameters.Add("@adress_id", System.Data.SqlDbType.Int).Value = adress_id;
+
+                    adapter.DeleteCommand.UpdatedRowSource = System.Data.UpdateRowSource.None;
+
+                    adapter.DeleteCommand.ExecuteNonQuery();
+
+                    connection.Close();
+                    return Request.CreateResponse(HttpStatusCode.OK, "Adress with id " + Convert.ToString(adress_id) + " deleted.");
+                }
+                else
+                {
+                    connection.Close();
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "There is no adress with id " + Convert.ToString(adress_id));
+                }
+
+            }
+        }
     }
 }
- }
--- day3/StudentApplicationSolution/StudentApplication/Controllers/StudentController.cs
+++ day3/StudentApplicationSolution/StudentApplication/Controllers/StudentController.cs
@@ -57,7 +57,7 @@ namespace StudentApplication.Controllers
                     connection.Close();
                     return Request.CreateErrorResponse(HttpStatusCode.NotFound, "There are no students");
                 }
-                
+
             }
         }
 
@@ -66,114 +66,216 @@ namespace StudentApplication.Controllers
 
 
 
-        // GET with id
+        // GET by id
         [HttpGet]
         [Route("api/Student/{Student_id}")]
         public HttpResponseMessage Get(int Student_id)
         {
             using (connection)
             {
+                connection.Open();
                 SqlCommand command = new SqlCommand
                     ("SELECT * FROM Student WHERE Student_id = @Student_id;", connection);
 
                 SqlParameter id_parameter = new SqlParameter("@Student_id", System.Data.SqlDbType.Int);
 
-                command.Parameters.Add(id_parameter);
-                connection.Open();
+                command.Parameters.Add(id_parameter).Value = Student_id;
+
 
 
                 SqlDataReader reader = command.ExecuteReader();
                 if (reader.HasRows)
                 {
+                    while (reader.Read())
+                    {
+                        Student student = new Student();
+                        student.student_id = Student_id;
+                        student.first_name = reader.GetString(1);
+                        student.last_name = reader.GetString(2);
+                        student.adress_id = reader.GetInt32(3);
 
-                    Student student = new Student();
-                    student.student_id = reader.GetInt32(0);
-                    student.first_name = reader.GetString(1);
-                    student.last_name = reader.GetString(2);
-                    student.adress_id = reader.GetInt32(3);
-
+                        list_of_students.Add(student);
+                    }
                     connection.Close();
 
-                    return Request.CreateResponse(HttpStatusCode.OK, student);
+                    return Request.CreateResponse(HttpStatusCode.OK, list_of_students);
                 }
                 else
                 {
                     connection.Close();
-                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "There is no student with id " + Student_id);
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "There is no student with id " + Convert.ToString(Student_id));
                 }
             }
         }
-
-    }
-}
-        /*
 
 
 
         // POST 
         [HttpPost]
-        [Route("api/Student")]
-        public HttpResponseMessage Post([FromBody] string value)
+        [Route("api/Student/{student_id}/{first_name}/{last_name}/{adress_id}")]
+        public HttpResponseMessage Post(int student_id, string first_name, string last_name, int adress_id)
         {
             using (connection)
-                SqlDataAdapter adapter = new SqlDataAdapter();
-                adapter.InsertCommand = new SqlCommand();
-                SqlCommand command = new SqlCommand
-                    ("INSERT INTO Student VALUES (@Student_id, @First_name, @Last_name, @Adress_id);", connection);
-
-                    command.Parameters.Add("@student_id", student_id);
-                    command.Parameters.Add("@First_name", first_name);
-                    command.Parameters.Add("@Last_name", last_name);
-                    command.Parameters.Add("@Adress_id", adress_id);
+            {
                 connection.Open();
+                string queryString = "INSERT INTO Student VALUES (@Student_id, @First_name, @Last_name, @Adress_id);";
 
-                adapter.Fill(data)
-                    
-            }
-        
-                return Request.CreateResponse(HttpStatusCode.OK, "Student is successfully added");
+                SqlCommand command = new SqlCommand(queryString, connection);
 
+                command.Parameters.Add("@Student_id", System.Data.SqlDbType.Int).Value = student_id;
+                command.Parameters.Add("@First_name", System.Data.SqlDbType.VarChar, 50).Value = first_name;
+                command.Parameters.Add("@Last_name", System.Data.SqlDbType.VarChar, 50).Value = last_name;
+                command.Parameters.Add("@Adress_id", System.Data.SqlDbType.Int).Value = adress_id;
+
+                SqlCommand command_check = new SqlCommand
+                   ("SELECT Adress_id FROM Student WHERE adress_id = @Adress_id;", connection);
+
+                SqlCommand command_check_pk = new SqlCommand
+                    ("SELECT Student_id FROM Student WHERE student_id = @Student_id;", connection);
+
+                command_check.Parameters.Add("@Adress_id", System.Data.SqlDbType.Int).Value = adress_id;
+                command_check_pk.Parameters.Add("@Student_id", System.Data.SqlDbType.Int).Value = student_id;
+
+
+                SqlDataReader reader = command_check.ExecuteReader();
+
+
+                if (reader.HasRows)
+                {
+                    connection.Close();
+                    connection.Open();
+                    SqlDataReader reader_pk = command_check_pk.ExecuteReader();
+                    if (!reader_pk.HasRows)
+                    {
+                        connection.Close();
+                        connection.Open();
+                        SqlDataAdapter adapter = new SqlDataAdapter();
+                        adapter.InsertCommand = command;
+                        adapter.InsertCommand.ExecuteNonQuery();
+                        connection.Close();
+                        return Request.CreateResponse(HttpStatusCode.OK, "Student is successfully added");
+                    }
+                    else
+                    {
+                        connection.Close();
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "You can not add student with already existing id.");
+
+                    }
+                }
+                else
+                {
+                    connection.Close();
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "You can not add student to non-existing adress.");
+
+                }
             }
-         
+
         }
-  }
-    }
-        // PUT 
+
+
+
+        // PUT for given id modifies adress of student
         [HttpPut]
-        [Route("api/Student/{id}")]
-        public HttpResponseMessage Put(int id, [FromBody] string value)
+        [Route("api/Student/{student_id}")]
+        public HttpResponseMessage Put(int student_id, [FromBody] int adress_id)
         {
-            if (DictionaryofStudents.students.Count() < id)
+            try
             {
-                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "There is no student with id: " + Convert.ToString(id));
+                using (connection)
+                {
+
+                    connection.Open();
+
+                    SqlCommand command_check_pk = new SqlCommand
+                            ("SELECT Student_id FROM Student WHERE student_id = @Student_id;", connection);
+
+                    command_check_pk.Parameters.Add("@Student_id", System.Data.SqlDbType.Int).Value = student_id;
+
+                    SqlDataReader reader_pk = command_check_pk.ExecuteReader();
+
+                    if (reader_pk.HasRows)
+                    {
+                        connection.Close();
+                        connection.Open();
+                        SqlDataAdapter adapter = new SqlDataAdapter("SELECT Student_id, Adress_id FROM Student", connection);
+                        adapter.UpdateCommand = new SqlCommand("UPDATE Student SET Adress_id = @Adress_id WHERE Student_id = @Student_id", connection);
+
+                        adapter.UpdateCommand.Parameters.Add("@Adress_id", System.Data.SqlDbType.Int).Value = adress_id;
+
+                        SqlParameter parameter = adapter.UpdateCommand.Parameters.Add("@Student_id", System.Data.SqlDbType.Int);
+                        parameter.SourceColumn = "Student_id";
+                        parameter.SourceVersion = System.Data.DataRowVersion.Original;
+
+                        System.Data.DataTable categoryTable = new System.Data.DataTable();
+                        adapter.Fill(categoryTable);
+
+                        System.Data.DataRow categoryRow = categoryTable.Rows[1];
+                        categoryRow["Adress_id"] = adress_id;
+
+
+
+                        adapter.Update(categoryTable);
+
+                        connection.Close();
+                        return Request.CreateResponse(HttpStatusCode.OK, "Student's adress is successfully modified");
+                    }
+                    else
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "There is no student with id " + Convert.ToString(student_id));
+                    }
+                }
+
             }
-            else
+            catch
             {
-                DictionaryofStudents.students[id] = value;
-                return Request.CreateResponse(HttpStatusCode.OK, "Student with id " + Convert.ToString(id) + " is modified");
+                return Request.CreateResponse(HttpStatusCode.NotFound, "There is no adress with id " + Convert.ToString(adress_id));
             }
         }
+
 
         // DELETE 
         [HttpDelete]
-        [Route("api/Student/{id}")]
-        public HttpResponseMessage Delete(int id)
+        [Route("api/Student/{student_id}")]
+        public HttpResponseMessage Delete(int student_id)
         {
-            if (DictionaryofStudents.students.Count() < id)
+            using (connection)
             {
-                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "There is no student with id: " + Convert.ToString(id));
-            }
-            else
-            {
-                DictionaryofStudents.students.Remove(id);
-                return Request.CreateResponse(HttpStatusCode.OK, "Student with id " + Convert.ToString(id) + " is deleted");
+                connection.Open();
+
+                SqlCommand command_check_pk = new SqlCommand
+                        ("SELECT Student_id FROM Student WHERE student_id = @Student_id;", connection);
+
+                command_check_pk.Parameters.Add("@Student_id", System.Data.SqlDbType.Int).Value = student_id;
+
+                SqlDataReader reader_pk = command_check_pk.ExecuteReader();
+
+                if (reader_pk.HasRows)
+                {
+                    connection.Close();
+                    connection.Open();
+
+                    SqlDataAdapter adapter = new SqlDataAdapter();
+
+                    adapter.DeleteCommand = new SqlCommand("DELETE FROM Student WHERE Student_id = @student_id ", connection);
+
+                    adapter.DeleteCommand.Parameters.Add("@student_id", System.Data.SqlDbType.Int).Value = student_id;
+
+                    adapter.DeleteCommand.UpdatedRowSource = System.Data.UpdateRowSource.None;
+
+                    adapter.DeleteCommand.ExecuteNonQuery();
+
+                    connection.Close();
+                    return Request.CreateResponse(HttpStatusCode.OK, "Student with id " + Convert.ToString(student_id) + " deleted.");
+                }
+                else
+                {
+                    connection.Close();
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "There is no student with id " + Convert.ToString(student_id));
+                }
+
             }
         }
     }
-
-
-
-
 }
 
-        */
+        
